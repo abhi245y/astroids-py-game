b06c26ac3a3caf7d6c5542e7d711037ea862094e
abhi245y <abhi245y@gmail.com>
2025-05-12 01:09:07 +0530

    added code for astroids and it's movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import *
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -8,10 +10,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField.containers = updatable
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -22,9 +28,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        updatable.update(dt)
+        for update in updatable:
+            update.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
