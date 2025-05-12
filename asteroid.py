879cbb32dfc9f2f9da045a0051afe5965b15a408
abhi245y <abhi245y@gmail.com>
2025-05-13 01:01:38 +0530

    Completed game logic and fucntion

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
+from constants import *
 import pygame
+import random
 
 
 class Asteroid(CircleShape):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle  = random.uniform(20,50)
+            velocity_plus = self.velocity.rotate(random_angle)
+            velocity_minus = self.velocity.rotate(-random_angle)
+            new_rad = self.radius - ASTEROID_MIN_RADIUS
+            
+            astroid_a = Asteroid(self.position.x, self.position.y, new_rad)
+            astroid_b = Asteroid(self.position.x, self.position.y, new_rad)
+
+            astroid_a.velocity = velocity_plus *1.2
+            astroid_b.velocity = velocity_minus *1.2
+            self.kill()
